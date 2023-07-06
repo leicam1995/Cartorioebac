edc14256437eeeab90d88f518001168889678c8f
leicam1995 <138815948+leicam1995@users.noreply.github.com>
2023-07-06 15:56:45 -0300

    Opção de saída.
    Incluso no sistema a opção de saída par o usuário.

--- cartorio.c
+++ cartorio.c
@@ -129,7 +129,8 @@ int main()
 	    printf("escolha a opção desejada do menu:\n\n");
 	    printf("\t1 Registrar nomes\n");
 	    printf("\t2 Consultar nomes\n");
-	    printf("\t3 Deletar nomes\n\n"); //fim do menu 
+	    printf("\t3 Deletar nomes\n"); //fim do menu 
+	    printf("\t4 Sair do sistema\n\n");
 	    printf("Este software pertence ao aluno Eduardo Maciel.\n");
 	    printf("Opção:");
 	    
@@ -152,6 +153,11 @@ int main()
 			case 3:
 			deletar();
 	    	break;
+	    	
+	    	case 4:
+	    	printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;	
 			
 			default:
 			printf("Essa opção não está disponivel!\n");
